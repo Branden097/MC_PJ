9e5bcda7ff3b1031ce92ce36fe7a051d63629f33
Branden <105219797+Branden097@users.noreply.github.com>
2022-07-26 10:57:08 +0800

    Update minecraft_log.py

--- minecraft_log.py
+++ minecraft_log.py
@@ -1,4 +1,3 @@
-# ! pip install gspread gspread-dataframe oauth2client gspread-formatting
 import pandas as pd
 import gspread
 from gspread_dataframe import set_with_dataframe
@@ -7,22 +6,16 @@ from oauth2client.service_account import ServiceAccountCredentials as sac
 import re
 import gzip
 
-# Set api key
 auth_json = 'minecraft-357407-fbea91beb3ee.json'
 
-# Set scopes
 gs_scopes = ['https://spreadsheets.google.com/feeds']
 
-# 以金鑰及操作範圍設定憑證資料
 cr = sac.from_json_keyfile_name(auth_json, gs_scopes)
 
-# 取得操作憑證
 gc = gspread.authorize(cr)
 
-# 以 url 開啟試算表
 gsheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1rCPCPSuPYjyDg4jfGbMVZmh-cOli4xoPIca5nwfvam4/edit#gid=0')
 
-# 選擇工作表1
 wks = gc.open_by_url('https://docs.google.com/spreadsheets/d/1rCPCPSuPYjyDg4jfGbMVZmh-cOli4xoPIca5nwfvam4/').worksheet("工作表2")
 
 def un_gz_rt_gs(file_name):
@@ -50,11 +43,9 @@ def un_gz_rt_gs(file_name):
 
     f.close()
 
-    # uses DEFAULT_FORMATTER
     fmt = cellFormat( horizontalAlignment = 'CENTER' )
     format_cell_range(wks, 'A:C', fmt)
 
-    # 寫入 Google Sheet
     set_with_dataframe(wks, df)
 
 un_gz_rt_gs('2022-07-23-1.log.gz') 
